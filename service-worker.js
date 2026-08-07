ae01287f98095824ba349cb781833ef1b17b6715
Hopes & Go <hopes-go@users.noreply.github.com>
2026-08-06 22:54:07 -0500

    Add temporary coming soon page

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hopes-go-v22";
+const CACHE_NAME = "hopes-go-v23-coming-soon";
 const APP_FILES = [
   "./",
   "./index.html",
@@ -34,5 +34,13 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))));
+    return;
+  }
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
